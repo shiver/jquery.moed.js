bf60aad3dda135e1085a4129e5ba6e8e58f708c1
Robert Vally <robert@vally.co.nz>
2011-12-16 19:10:29 +1300

    Experimenting with event system

--- jquery.moed.js
+++ jquery.moed.js
@@ -16,21 +16,85 @@
 	var edId = 0;
 	var Edit = function($e, options) {
 		var id = ++edId;
+		$e.addClass('jqedit').hide();
 
-		$e.addClass('jqedit original').hide();
 
-		$e.after($('<div/>').after('<input></input>')
-			.filter('div')
-				.attr('id', 'jqedit-preview-' + id)
-				.html($e.val())
-				.on('dblclick', function() {
-					$e.trigger('edit.jqedit', [this]);
+		function createElements() {
+			$e.after($('<div/>').after('<input></input>')
+				.filter('div')
+					.attr('id', 'jqedit-preview-' + id)
+					.addClass('preivew')
+					.html($e.val()) 
+				.end() 
+				.filter('input')
+					.attr('id', 'jqedit-input-' + id)
+					.addClass('input')
+					.val($e.val())
+					.hide()
+				.end().after($('<ul/>')
+					.attr('id', 'jqedit-dropdown-' + id)
+					.addClass('dropdown')
+				)
+			);
+
+			var dd = $('#jqedit-dropdown-' + id).hide();
+			$e.find('option').each(function() {
+				dd.append($('<li value="'+this.value+'">'+this.text+'</li>'));
+			});
+		}
+
+		function registerEvents() {
+			$('#jqedit-preview-' + id)
+				.on('mouseenter mouseleave', function(e) {
+					$e.trigger('previewHover.jqedit', [this]);
 				})
-			.end() 
-			.filter('input')
-				.attr('id', 'jqedit-input-' + id)
-				.val($e.val())
-			.end()
-		);
+				.on('dblclick', function(e) {
+					e.preventDefault();
+					$e.trigger('startEdit.jqedit', [this]);
+					$e.trigger('focusInput.jqedit', [this]);
+				}
+			);
+
+			$('#jqedit-input-' + id)
+				.on('keypress', function(e) {
+					console.log('keypress: ' + e.which);
+				})
+				.on('keydown', function(e) {
+					console.log('keydown: ' + e.which);
+					switch (e.which) {
+						case 27:
+							if (!$e.data('editing')) break;
+							$e.trigger('stopEdit.jqedit', [this]);
+							break;
+						case 13:
+							if (!$e.data('editing')) break;
+							$e.trigger('submit.jqedit', [this]);
+							break;
+					}
+				}
+			);
+			$('#jqedit-dropdown-' + id);
+
+			// Externally triggered to update various plugin components
+			$e.on('startEdit.jqedit', function(e, opt) {
+				if ($e.data('editing')) return;
+				$('#jqedit-preview-' + id).hide();		
+				$('#jqedit-input-' + id).show();		
+				$e.data('editing', true);
+			}).on('stopEdit.jqedit', function(e, opt) {
+				if (!$e.data('editing')) return;
+				$('#jqedit-input-' + id).hide();		
+				$('#jqedit-preview-' + id).show();
+				$e.data('editing', false);
+			}).on('focusInput.jqedit', function(e, opt) {
+				if (!$e.data('editing')) return;
+				$('#jqedit-input-' + id).focus();
+			}).on('previewUpdate.jqedit', function(e, opt) {
+				console.log(opt.display + ' ' + opt.value);
+			});
+		}
+
+		createElements();
+		registerEvents();
 	};
 }(jQuery));
